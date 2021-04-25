a5106a3f32b396881f84e78ff92b0a735b014d22
Caleb C <cbcunningham20@gmail.com>
2021-04-24 21:01:15 -0500

    Finished the GoldDivider.java so that it works with proper bit accuracy, and prints binary.

--- goldDivider.java
+++ goldDivider.java
@@ -1,7 +1,7 @@
 
 public class goldDivider {
   public static void main(String[] args) {
-      double numerator, denominator, kValue, remainder, quotient, percision, finalPercision, scale, ulp;
+      double numerator, denominator, kValue, trueRemainder, iterativeRemainder, quotient, percision, finalPercision, scale, ulp;
       int iterations, internalPercision, outputPercision;
 
       if (args.length < 6) {
@@ -11,27 +11,48 @@ public class goldDivider {
 
       numerator = Double.parseDouble(args[0]);
       denominator = Double.parseDouble(args[1]);
-      //kValue = Double.parseDouble(args[2]);
-      kValue = 0.75;
+      kValue = Double.parseDouble(args[2]);
       iterations = Integer.parseInt(args[3]);
       percision = Double.parseDouble(args[4]);
       finalPercision = Double.parseDouble(args[5]);
 
-      /*internalPercision = (int) percision;
+      internalPercision = (int) percision;
       outputPercision = (int) finalPercision;
 
+      // Make sure our values only go to 15 fractional binary places
       numerator = roundDouble(numerator, internalPercision);
-      denominator = roundDouble(denominator, internalPercision);*/
+      denominator = roundDouble(denominator, internalPercision);
 
+      // Calculate Code Value for final Comparison
       quotient = numerator / denominator;
-      double rI = 0;
-      for (int i = 0; i < iterations; i++) {
+
+      double ri = denominator;
+      double ni = numerator;
+      int i;
+      for (i = 0; i < iterations; i++) {
+        //System.out.printf("K%d: %.15f, Numerator: %.15f, Denominator: %.15f\n", i, kValue,numerator,denominator);
+        System.out.printf("K%d: %.15f, Numerator: %.15f, Denominator: %.15f, Numerator in Binary: %s\n", i, kValue,ni,ri,toBinary(ni,15));
+        ni = ni * kValue;
+        ri = ri * kValue;
+        kValue = 2 - ri;
+
+        /*
         numerator = numerator * kValue;
         denominator = denominator * kValue;
         kValue = 2 - denominator;
+        */
       }
+      //System.out.printf("K%d: %.15f, Numerator: %.15f, Denominator: %.15f\n", i, kValue,numerator,denominator);
+      System.out.printf("K%d: %.15f, Numerator: %.15f, Denominator: %.15f, Numerator in Binary: %s\n", i, kValue,ni,ri,toBinary(ni,15));
 
-      System.out.printf("Quotient by Code: %.15f, Iterations: %d, Quotient by iteration: %.15f, Percent Error= %.2f",quotient, iterations, numerator, percentError(quotient, numerator));
+      iterativeRemainder = Math.pow(2,15) * ((quotient * denominator) - numerator);
+      trueRemainder = Math.pow(2,15) * ((quotient * denominator) - numerator);
+
+      //Answer Clean up
+      iterativeRemainder = floorDouble(iterativeRemainder, 15);
+      trueRemainder = roundDouble(ni, 15);
+
+      System.out.printf("Quotient by Code: %.15f, with remainder %.15f, Iterations: %d, \nQuotient by iteration: %.15f, with remainder remainder %.15f, Percent Error= %.5f",quotient, trueRemainder, iterations, ni ,iterativeRemainder, percentError(quotient, ni));
 
       //System.out.printf("N=%f,D=%f,Init=%f,Iterations=%d,Prec=%f,FinalPerc=%f",numerator, denominator,initialValue,iterations,percision,finalPercision);
   }
@@ -52,5 +73,28 @@ public class goldDivider {
   private static double ceilingDouble(double toCeiling, int binaryPlaces) {
     double scale = Math.pow(2, binaryPlaces);
     return Math.ceil(toCeiling * scale) / scale;
+  }
+
+  // Code I found online to make this process easy
+  private static String toBinary(double d, int precision) {
+      long wholePart = (long) d;
+      return wholeToBinary(wholePart) + '.' + fractionalToBinary(d - wholePart, precision);
+  }
+  private static String wholeToBinary(long l) {
+      return Long.toBinaryString(l);
+  }
+  private static String fractionalToBinary(double num, int precision) {
+      StringBuilder binary = new StringBuilder();
+      while (num > 0 && binary.length() < precision) {
+          double r = num * 2;
+          if (r >= 1) {
+              binary.append(1);
+              num = r - 1;
+          } else {
+              binary.append(0);
+              num = r;
+          }
+      }
+      return binary.toString();
   }
 }
